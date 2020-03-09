6786e3539a26ef5c4f80d1a32646bfbc0619ca4d
maguimarijuan <maguimar001@gmail.com>
2020-03-09 15:51:54 -0300

    add validate type and missing data tests

--- tests/api-get-upload-data.js
+++ tests/api-get-upload-data.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const ApiTest = require('@janiscommerce/api-test');
+// const path = require('path');
+const { ApiGetUploadData } = require('../lib/index');
+
+
+describe('API getUploadData', () => {
+
+	context('When validating type and missing data  ', () => {
+		ApiTest(ApiGetUploadData, '/api/getUploadData', [
+			{
+				description: 'Should return 400 if the required field \'entity\' is not passed',
+				request: {
+					data: { entity: undefined, fileName: 'some-file-name.xls' }
+				},
+				session: true,
+				response: {
+					code: 400
+				}
+			},
+			{
+				description: 'Should return 400 if the required field \'filename\' is not passed',
+				request: {
+					data: { entity: 'fakeEntity', fileName: undefined }
+				},
+				session: true,
+				response: {
+					code: 400
+				}
+			},
+			{
+				description: 'Should return 400 if the required field \'filename\' is not a valid file name',
+				request: {
+					data: { entity: 'fakeEntity', fileName: 'some-file-name.pdf' }
+				},
+				session: true,
+				response: {
+					code: 400
+				}
+			}
+		]);
+	});
+});
